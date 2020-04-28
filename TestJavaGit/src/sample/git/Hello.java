860f6fcf247f2839d776acc9bfa199ea44e45bb4
Ron Weiner <rbweiner1@comcast.net>
2020-04-27 22:31:15 -0500

    Corrected file

--- TestJavaGit/src/sample/git/Hello.java
+++ TestJavaGit/src/sample/git/Hello.java
@@ -3,7 +3,7 @@ package sample.git;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to First Git program by RonW);
+		System.out.println("Welcome to First Git program by RonW");
 
 	}
 
